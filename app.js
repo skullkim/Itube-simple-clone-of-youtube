b0349dfdc8e3f2f3e4d1a2daf7bea514c7d76f54
김윤기 <iskull@gim-yungiui-MacBookPro.local>
2021-01-26 21:24:28 +0900

    finished front-size signtup page, added logged in-status header

--- app.js
+++ app.js
@@ -38,13 +38,13 @@ app.use(session({
 
 const index_router = require('./routes');
 const login_router = require('./routes/login');
-//const signup_router = require('./routes/signup');
+const signup_router = require('./routes/signup');
 
 app.use('/style', express.static('public'));
 app.use('/script', express.static('public'));
 app.use('/', index_router);
 app.use('/login', login_router);
-//app.use('/signup', signup_router);
+app.use('/signup', signup_router);
 
 app.use((req, res, next) => {
     const error = new Error(`${res.method} ${req.url} router doesn't exist`);
--- routes/signup.js
+++ routes/signup.js
@@ -2,11 +2,9 @@ const express = require('express');
 const path = require('path');
 
 const router = express.Router();
-
 router.get('/', (req, res, next) => {
     try{
-        //res.render('index', {is_logged_in: true});
-        res.render('index');
+        res.render('signup');
     }
     catch(err){
         console.error(err);
